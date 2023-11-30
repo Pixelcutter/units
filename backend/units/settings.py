90a1cb9fa1ee2be50e86b9061d866fd76017454a
pixelcutter <nwmusicview@gmail.com>
2023-11-29 16:25:35 -0800

    deleted category and user apps. Will implement them in the future, if I need to.

--- backend/units/settings.py
+++ backend/units/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', # Django REST Framework
+    'product', # Product app
+    'units_api', # Units API app
 ]
 
 MIDDLEWARE = [
