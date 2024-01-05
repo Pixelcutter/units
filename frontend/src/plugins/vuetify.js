2adc138be9ce0911765b34d156aabfea95cea5a8
pixelcutter <nwmusicview@gmail.com>
2024-01-04 17:42:00 -0800

    added material design 3 blueprint

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -10,8 +10,9 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { md3 } from 'vuetify/blueprints'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  //
+  blueprint: md3, // material design 3
 })
